f2ca66e182e4e33eb6a011b4ef3220ccfd844c57
bongohead <charles@macropredictions.com>
2025-03-10 15:35:00 -0400

    Initial commit

---
+++ install_packages.ps1
@@ -0,0 +1,18 @@
+# Install necessary packages
+pip install torch==2.6.0 --index-url https://download.pytorch.org/whl/cu126
+pip install bitsandbytes==0.45.2
+pip install transformers==4.48.3
+pip install accelerate==1.3.0
+pip install triton==3.1.0
+pip install flash-attn==2.7.4.post1 --no-build-isolation
+pip install jupyter lab
+pip install plotly.express
+pip install wandb
+pip install pyyaml
+pip install pyarrow
+pip install termcolor
+pip install pandas
+pip install tqdm
+pip install sqlalchemy
+pip install python-dotenv
+pip install datasets
